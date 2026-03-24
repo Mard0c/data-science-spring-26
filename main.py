fa2d42574b78076ab789d368487240ffa54d5613
Mard0c <dtmss11@gmail.com>
2026-03-24 13:05:17 +0100

    fix: I should not be let near a computer

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import xmltodict
 def main():
 
     # open the file
-    fileptr = open("sample-input/260100023-773287000-2024-xml copy.xml", "r")
+    fileptr = open("input/xml_2024/510841096-772822000-2024-xml.xml", "r")
 
     # read xml content from the file
     xml_content = fileptr.read()
@@ -14,10 +14,10 @@ def main():
     my_ordered_dict = xmltodict.parse(xml_content)
 
     # select relevant data
-    selected_hospital_data = select_hospital_data(my_ordered_dict)
+    selected_hospital_data = select_hospital_data(my_ordered_dict["Qualitaetsbericht"])
 
     # (temporary) print data
-    print(selected_hospital_data["Qualitaetsbericht"])
+    print(selected_hospital_data)
 
 
 def select_hospital_data(all_hospital_data: dict) -> dict:
@@ -27,33 +27,33 @@ def select_hospital_data(all_hospital_data: dict) -> dict:
     try:
         result["Datum"] = all_hospital_data["Einleitung"]["Datensatz"]["Datum"]
     except KeyError:
+        print("ERROR: could not find 'Datum'")
         result["Datum"] = None
 
     # Krankenhaus/.../Postleitzahl
+    # Can have multiple bloody locations...
+    # It's either Krankenhaus/Mehrere_Standorte
+    # or          Krankenhaus/Ein_Standorte
     try:
-        result["Postleitzahl"] = all_hospital_data["Krankenhaus"]["Mehrere_Standorte"][
+        result["Postleitzahl"] = all_hospital_data["Krankenhaus"]["Ein_Standort"][
             "Krankenhauskontaktdaten"
         ]["Kontakt_Adresse"]["Postleitzahl"]
     except KeyError:
+        print("ERROR: could not find 'Postleitzahl'")
         result["Postleitzahl"] = None
 
     # Anzahl_Betten
     try:
         result["Anzahl_Betten"] = all_hospital_data["Anzahl_Betten"]
     except KeyError:
+        print("ERROR: could not find 'Anzahl_Betten'")
         result["Anzahl_Betten"] = None
 
     # Fallzahlen (all child elements)
     try:
         result["Fallzahlen"] = all_hospital_data["Fallzahlen"]
-        fallzahlen_raw = all_hospital_data["Fallzahlen"]
-        # Keep only children whose value looks numeric
-        result["Fallzahlen"] = {
-            k: v
-            for k, v in fallzahlen_raw.items()
-            if v is not None and str(v).lstrip("-").isdigit()
-        }
     except KeyError:
+        print("ERROR: could not find 'Fallzahlen'")
         result["Fallzahlen"] = {}
 
     return result
