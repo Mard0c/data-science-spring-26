c19c86ead82f66219364e33e1f2976178de56b23
Mard0c <dtmss11@gmail.com>
2026-03-27 16:09:18 +0100

    refactor: prepared for processing multiple files.

--- main.py
+++ main.py
@@ -3,24 +3,27 @@ import xmltodict
 
 
 def main():
+    process_one_hospital("input/xml_2024/510841096-772822000-2024-xml.xml")
 
-    # open the file
-    fileptr = open("input/xml_2024/510841096-772822000-2024-xml.xml", "r")
 
-    # read xml content from the file
-    xml_content = fileptr.read()
+# "input/xml_2024/510841096-772822000-2024-xml.xml"
+def process_one_hospital(path):
 
-    # change xml format to ordered dict
-    my_ordered_dict = xmltodict.parse(xml_content)
+    # open the file as a string
+    xml_content = open(path, "r").read()
 
     # select relevant data
-    selected_hospital_data = select_hospital_data(my_ordered_dict["Qualitaetsbericht"])
+    selected_hospital_data = select_hospital_data(xml_content)
 
     # (temporary) print data
     print(selected_hospital_data)
 
 
-def select_hospital_data(all_hospital_data: dict) -> dict:
+def select_hospital_data(xml: str) -> dict:
+    # translate xml to dictionary
+    all_hospital_data = xmltodict.parse(xml)["Qualitaetsbericht"]
+
+    # collect target data in this dictionary
     result = {}
 
     # Einleitung/Datensatz/Datum
@@ -33,13 +36,17 @@ def select_hospital_data(all_hospital_data: dict) -> dict:
     # Krankenhaus/.../Postleitzahl
     # Can have multiple bloody locations...
     # It's either Krankenhaus/Mehrere_Standorte
-    # or          Krankenhaus/Ein_Standorte
+    #          or Krankenhaus/Ein_Standorte
     try:
         result["Postleitzahl"] = all_hospital_data["Krankenhaus"]["Ein_Standort"][
             "Krankenhauskontaktdaten"
         ]["Kontakt_Adresse"]["Postleitzahl"]
     except KeyError:
         print("ERROR: could not find 'Postleitzahl'")
+
+        if "Mehrere_Standorte" in all_hospital_data["Krankenhaus"]:
+            print("UNHANDLED EDGE CASE: has multiple locations")
+
         result["Postleitzahl"] = None
 
     # Anzahl_Betten
@@ -60,4 +67,5 @@ def select_hospital_data(all_hospital_data: dict) -> dict:
 
 
 if __name__ == "__main__":
+    # paths = [str(p) for p in Path("input_files").rglob("*.txt")]
     main()
