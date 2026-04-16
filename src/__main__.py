2639d4bc3fc60afa9994f6ffa4810660e2293bf9
Mard0c <dtmss11@gmail.com>
2026-04-16 21:52:20 +0200

    refactor: preparing for real ingestion

--- src/__main__.py
+++ src/__main__.py
@@ -7,14 +7,19 @@ def main():
     hospital_data = hospitalxml.retrieve_target_data("input/xml_2023", "*xml.xml")
     databasecontroller.insert_test_data(hospital_data)
     databasecontroller.show_db_contents("sample")
+    # add some kind of cli tool?
 
 
 def add_one_year(year):
     year -= 1
 
     hospital_data = hospitalxml.retrieve_target_data(f"input/xml_{year}", "*xml.xml")
-    databasecontroller.insert_test_data(hospital_data)
+    databasecontroller.insert_data(hospital_data)
     databasecontroller.show_db_contents("sample")
+
+
+def test(input):
+    print(input)
 
 
 if __name__ == "__main__":
--- src/databasecontroller.py
+++ src/databasecontroller.py
@@ -29,6 +29,34 @@ def insert_test_data(data):
     db.close()
 
 
+def insert_data(data):
+
+    # replace with a in memory database first?
+    db = duckdb.connect("database.db")
+
+    arrow_table = pyarrow.Table.from_pylist(data)
+    print(type(arrow_table["Datum"][0]))
+    db.register("data_view", arrow_table)
+
+    # might be useful:
+    # tables = [t[0] for t in db.execute("SHOW TABLES").fetchall()]
+    # print(tables)
+    table_exists = (
+        db.sql(
+            "SELECT * FROM information_schema.tables WHERE table_name = 'sample'"
+        ).fetchall()
+        != []
+    )
+    print(f"table_exists: {table_exists}")
+
+    if not table_exists:
+        db.sql("CREATE TABLE sample AS SELECT * FROM data_view")
+    else:
+        # db.sql("TRUNCATE TABLE sample")
+        db.sql("INSERT INTO sample SELECT * FROM data_view")
+    db.close()
+
+
 def show_db_contents(table: str):
     db = duckdb.connect("database.db")
     try:
